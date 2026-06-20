a3ee8c97e2d57e5dd9384594c4ed4aafb4041a79
Mahir <mahirjambhule92@gmail.com>
2026-06-20 20:17:40 +0530

    Initial commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const isLocalhost =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: isLocalhost
+    ? "http://localhost:5000/api"
+    : "https://mvault-hng8.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
